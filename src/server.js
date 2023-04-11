6482431c8eec5fce2d0007c86518602be51d2203
HICHULOG <itchu92@gmail.com>
2023-04-11 12:10:35 +0900

    Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
+
 const handleHome = (req, res) => {
-    return res.send("I still love you.");
-}
+    return res.send("I love middlewares");
+};
 
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-}
+app.get("/", gossipMiddleware, handleHome);
 
-app.get("/", handleHome)
-app.get("/login", handleLogin)
-
-app.listen(PORT, () => console.log(`Server listening on port http://localhost:${PORT} 🚀`));  
+app.listen(PORT, () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀.`));
