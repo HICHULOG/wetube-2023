1c03ce592b800886c3a047240a0dab417bfcdbec
HICHULOG <itchu92@gmail.com>
2023-04-17 17:16:12 +0900

    Pug Iteration Array

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,9 +2,10 @@ const fakeUser = {
     username: "HICHULOG",
     loggedIn: true
 }
-
-
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser});
+export const trending = (req, res) => {
+    const videos = [1,2,3,4,5,6,7,8,9,10];
+    return res.render("home", {pageTitle: "Home", fakeUser, videos});
+} 
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch Video"});
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
