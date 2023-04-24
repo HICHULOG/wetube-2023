9e638fa04db684f5e4f9f45747d481c3b20588e8
HICHULOG <itchu92@gmail.com>
2023-04-24 15:47:50 +0900

    returns and renders

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,20 +1,19 @@
 import Video from "../models/Video"
 
 /*
-    Video.find({}, (error, videos) => {
-        if(erroe){
+    console.log("started")
+     Video.find({}, (error, videos) => {
+        if(error) {
             return res.render("server-error")
         }
-        return res.render("home", {pageTitle: "Home", videos});
-    }); 
+        return res.render("home", {pageTitle: "Home", videos})
+    });
+    console.log("finished")
 */
+
 export const home = async(req, res) => {
-    try{
-        const videos = await Video.find({});
-        return res.render("home", {pageTitle: "Home", videos});
-    } catch {
-        return res.render("server-error");
-    }
+    const videos = await Video.find({});
+    return res.render("home", {pageTitle: "Home", videos})
 };
 export const watch = (req, res) => {
     const { id } = req.params;
