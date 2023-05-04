725aa40cfe3ceb1cf800309f54f08ac8424fe4ac
HICHULOG <itchu92@gmail.com>
2023-05-04 14:12:00 +0900

    Edit Video part Three

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,15 +36,17 @@ export const getEdit = async(req, res) => {
 export const postEdit = async(req, res) => {
     const { id } = req.params;
     const { title, description, hashtags } = req.body;
-    const video = await Video.findById(id);
+    const video = await Video.exists({_id: id});
     if(!video){
         return res.render("404", {pageTitle: "Video not found."});
     }
-    video.title = title;
-    video.description = description;
-    video.hashtags = hashtags
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: hashtags
         .split(",")
-        .map( (word) => (word.startsWith('#') ? word : `#${word}`));
+        .map( (word) => (word.startsWith('#') ? word : `#${word}`))
+    }) 
     await video.save();
     return res.redirect(`/videos/${id}`);
 };
