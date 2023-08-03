408be056b2c85447027a682d0237101405985134
HICHULOG <itchu92@gmail.com>
2023-08-03 20:00:21 +0900

    GET Requests

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = 4000;
 
+app.get("/", () => console.log("Somebody is trying to go home."));
+
 const handleListening = () => 
     console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
