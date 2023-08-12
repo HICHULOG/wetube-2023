2d2ce1431f2ef8ccbbbda54160daa1bf2aa0221e
HICHULOG <itchu92@gmail.com>
2023-08-12 21:49:39 +0900

    Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,25 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-    // 최신버전 mongoose에서는 Model.find()에서 callback함수를 지원하지 않는다.
-    // mongoose를 다운그레이드하거나 promise를 사용해야한다.
-    Video.find({}, (error, videos) => {
-        res.render("home", { pageTitle: "Home", videos: [] });
-    });
+/* 
+
+Video.find({}, (error, videos) => {
+    if(error){
+        return res.render("server-error")
+    }
+    return res.render("home", { pageTitle: "Home", videos })
+});
+
+*/
+
+export const home = async(req, res) => {
+    try{
+        const videos = await Video.find({});
+        return res.render("home", { pageTitle: "Home", videos: [] });
+    } catch {
+        return res.render("server-error");
+    }
 };
 export const watch = (req, res) => {
-    // const id = req.params.id;은 아래 표현식과 같은 표현이다.
     const { id } = req.params;
     res.render("watch", { pageTitle: `Watching` });
 };
