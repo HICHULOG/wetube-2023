738667d13ecf3c7419fabd5716f7eb73c9f7efa1
HICHULOG <itchu92@gmail.com>
2023-08-18 15:43:11 +0900

    Login part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,16 +17,36 @@ export const postJoin = async(req, res) => {
             errorMessage: "This username/email is already taken."
         });
     }
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
-    });
-    return res.redirect("/login");
+    try{
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        });
+        return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMessage: error._message,
+        });
+    }
 };
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
+export const postLogin = async(req, res) => {
+    const { username, password } = req.body;
+    // check if account exists
+    const exists = await User.exists({username});
+    if(!exists){
+        return res.status(400).render("login", { 
+            pageTitle: "Login",
+            errorMessage: "An account with this username does not exists.",
+        });
+    }
+    // check if password correct
+    return res.end();
+};
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See User");
 export const edit = (req, res) => res.send("Edit User");
